bce6e12c57e63081064c4f143d99ecc4063c2674
Sam Ross <sam@rosss.dev>
2021-10-18 20:20:36 +0000

    remove testing

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import DiscordJS, { Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const guildID = '899527592892330027'
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -11,36 +12,6 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('The bot is ready.')
-
-    const guildID = '899527592892330027'
-    const guild = client.guilds.cache.get(guildID)
-    let commands
-
-    if (guild) {
-        commands = guild.commands
-    } else {
-        commands = client.application?.commands
-    }
-
-    commands?.create({
-        name: 'ping',
-        description: 'Replys with pong.',
-    })
-})
-
-client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) {
-        return
-    }
-
-    const { commandName, options } = interaction
-
-    if (commandName === 'ping') {
-        interaction.reply({
-            content: 'pong',
-            ephemeral: true,
-        })
-    }
 })
 
 
